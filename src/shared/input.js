aaf750e91d08e84a7a5504a7fcd94831099e0c37
Aurio Robles <alejandro.ro94@gmail.com>
2022-08-19 19:25:06 -0500

    improve shared input

--- src/shared/input.js
+++ src/shared/input.js
@@ -6,12 +6,28 @@ const Fieldset = styled.fieldset`
 
 const Label = styled.label``;
 const Input = styled.input``;
+const InfoP = styled.p`
+  white-space: pre-line;
+`;
 
-const WrappedInput = (props) => (
-  <Fieldset>
-    <Label>{props.label}</Label>
-    <Input defaultValue={props.value} onChange={props.onChange} />
-  </Fieldset>
-);
+const WrappedInput = (props) => {
+  const infoPId = `info-${props.id}`;
+  return (
+    <Fieldset>
+      <Label htmlFor={props.id}>{props.label}</Label>
+      <Input
+        id={props.id}
+        defaultValue={props.value}
+        onChange={props.onChange}
+        aria-describedby={`${infoPId}`}
+      />
+      {props.info ? (
+        <InfoP id={infoPId} aria-live='polite'>
+          {props.info}
+        </InfoP>
+      ) : null}
+    </Fieldset>
+  );
+};
 
 export default WrappedInput;
