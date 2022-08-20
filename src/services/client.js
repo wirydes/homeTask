6d2b8e7814a5c178d2bc2903d094c6efa0a4c35a
Aurio Robles <alejandro.ro94@gmail.com>
2022-08-20 17:48:19 -0500

    improve InMemoryCache

--- src/services/client.js
+++ src/services/client.js
@@ -19,7 +19,20 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Topic: {
+        stargazers: {
+          edges: {
+            keyFields: ['node'],
+            node: {
+              keyFields: ['id'],
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
